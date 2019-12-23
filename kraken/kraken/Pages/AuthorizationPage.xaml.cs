8cfce6c6f143a79385384df9d35d638fe80a1214
fastandfurios <semu-dima@yandex.ru>
2019-12-23 10:05:51 +0300

    Add project files.

---
+++ kraken/kraken/Pages/AuthorizationPage.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace kraken.Pages
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class AuthorizationPage : ContentPage
+    {
+        public AuthorizationPage()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// Выполняет переход на страницу регистрации
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void TapGestureRecognizer_Tapped(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new RegistrationPage());
+        }
+
+        /// <summary>
+        /// Выполняет переход на страницу профиля
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new MyProfilePage());
+        }
+    }
+}
---
+++ kraken/kraken/Pages/RegistrationPage.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace kraken.Pages
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class RegistrationPage : ContentPage
+    {
+        public RegistrationPage()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// Выполняет переход на страницу профиля 
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new MyProfilePage());
+        }
+
+        /// <summary>
+        /// Выполняет переход на страницу авторизации
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Button_Clicked_1(object sender, EventArgs e)
+        {
+            Navigation.PopAsync();
+        }
+    }
+}
