af9fcd899fae97ec89f053485279d82f5bd46bd1
fastandfurios <49958503+fastandfurios@users.noreply.github.com>
2020-04-28 14:26:38 +0300

    :apple: update app.xaml.cs

--- kraken/kraken/App.xaml.cs
+++ kraken/kraken/App.xaml.cs
@@ -48,23 +48,10 @@ namespace kraken
             ExtendedTabbedPage tabbedNavigation = new ExtendedTabbedPage(NavigationContainerNames.MainContainer);
             tabbedNavigation.On<Xamarin.Forms.PlatformConfiguration.Android>().SetToolbarPlacement(ToolbarPlacement.Bottom);
 
-            var tabIconProfile = "ic_action_person.png";
-            var tabIconCreateRequests = "ic_action_note_add.png";
-            var tabIconMyRequest = "ic_action_list_alt.png";
-            var tabIconExit = "ic_action_input.png";
-
-            if (Device.iOS == Device.RuntimePlatform)
-            {
-                tabIconProfile = "baseline_person_black_24pt_1x.png";
-                tabIconCreateRequests = "baseline_note_add_black_24pt_1x.png";
-                tabIconMyRequest = "baseline_list_alt_black_24pt_1x.png";
-                tabIconExit = "baseline_input_black_24pt_1x.png";
-            }
-
-            tabbedNavigation.AddTab<MyProfilePageModel>("Профиль", tabIconProfile, null);
-            tabbedNavigation.AddTab<CreateRequestPageModel>("Создать запрос", tabIconCreateRequests, null);
-            tabbedNavigation.AddTab<MyRequestPageModel>("Мои запросы", tabIconMyRequest, null);
-            tabbedNavigation.AddTab<ExitPageModel>("Выход", tabIconExit, null);
+            tabbedNavigation.AddTab<MyProfilePageModel>("Профиль", "ic_action_person.png", null);
+            tabbedNavigation.AddTab<CreateRequestPageModel>("Создать запрос", "ic_action_note_add.png", null);
+            tabbedNavigation.AddTab<MyRequestPageModel>("Мои запросы", "ic_action_list_alt.png", null);
+            tabbedNavigation.AddTab<ExitPageModel>("Выход", "ic_action_input.png", null);
 
             tabbedNavigation.SelectedTabColor = Color.Red;
             tabbedNavigation.UnselectedTabColor = Color.FromHex("#9E9E9E");
